a28c50c700d77b6394f4352d63a1c93ce3f3b9f7
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-06 13:47:45 -0500

    Add imports and dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import axios from 'axios';
+import { createStore } from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects'
+
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+
+};
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+);
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(<App />, document.getElementById('root'));
